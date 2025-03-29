8c12721fbae4d364a2015b5980cbb3b0b2b9028f
Joylinton04 <adujoy05@gmail.com>
2025-03-29 10:50:15 +0000

    app setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		fontFamily: {
+  			default: 'Open Sans, sans-serif'
+  		},
+  		colors: {
+  			secwhite: '#eaeaea'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+		padding: {
+			padLR: '5rem'
+		},
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
